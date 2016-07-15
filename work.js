f755d9ce2db0b633bfc80559452d5868249f19a3
KashaevaDasha <kashaeva1998@list.ru>
2016-07-15 13:17:28 +0300

    homework_8

--- work.js
+++ work.js
@@ -1,29 +1,90 @@
-/*Напишите функцию sum, которая будет работать так:
-               sum(1)(2) == 3;*/
-function sum_1(variable) {
-	var result = variable;
-	function current(a) {
-		result += a;
-		return current;
-	}
-	current.toString = function() {
-		return result;
+/*Напишите функцию-конструктор Calculator, которая создает объект с тремя методами:
+Метод read() запрашивает два значения при помощи prompt и запоминает их в свойствах объекта.
+Метод sum() возвращает сумму запомненных свойств.
+Метод mul() возвращает произведение запомненных свойств.*/
+
+function Calculator() {
+	this.read = function() {
+		this.a = +prompt('1 = ');
+		this.b = +prompt('2 = ');
 	};
-	return current;
+	this.sum = function() {
+		return this.a + this.b;
+	};
+	this.mul = function() {
+		return this.a * this.b;
+	};
 }
 
-/*Мне было весело :D*/
-function sum_2(variable) {
-	return (function (current) {
-		current.toString = function () {
-			return variable;
-		};
-		return current;
-	})(function current(a) {
-		variable += a;
-		return current;
-	});
-}
+var calculator = new Calculator();
 
-console.log(sum_1(12)(3)(1));
-console.log(sum_2(12)(2)(4));
+calculator.read();
+console.log("Сумма=" + calculator.sum());
+console.log("Произведение=" + calculator.mul());
+
+/*Напишите функцию-конструктор Accumulator(startingValue).
+Объекты, которые она создает, должны хранить текущую сумму и прибавлять к ней то, что вводит посетитель.
+Более формально, объект должен:
+	Хранить текущее значение в своём свойстве value.
+Начальное значение свойства value ставится конструктором равным startingValue.
+	Метод read() вызывает prompt, принимает число и прибавляет его к свойству value.
+	Таким образом, свойство value является текущей суммой всего,
+что ввел посетитель при вызовах метода read(), с учетом начального значения startingValue.*/
+
+function Accumulator(startingValue) {
+	this.value = startingValue;
+	this.read = function() {
+		this.value += +prompt('Введите число = ');
+	};
+}
+var accumulator = new Accumulator(1); // начальное значение 1
+accumulator.read(); // прибавит ввод prompt к текущему значению
+accumulator.read(); // прибавит ввод prompt к текущему значению
+console.log(accumulator.value); // выведет текущее значение
+
+/*	Напишите конструктор Calculator, который создаёт расширяемые объекты-калькуляторы.
+	Эта задача состоит из двух частей, которые можно решать одна за другой.
+	Первый шаг задачи: вызов calculate(str) принимает строку, например «1 + 2»
+с жёстко заданным форматом «ЧИСЛО операция ЧИСЛО» (по одному пробелу вокруг операции),
+и возвращает результат. Понимает плюс + и минус -.
+	Второй шаг – добавить калькулятору метод addMethod(name, func), который учит калькулятор новой операции.
+Он получает имя операции name и функцию от двух аргументов func(a,b), которая должна её реализовывать.*/
+
+function Calculator2() {
+	var symbols = {
+		"-": function(a, b) {
+			return a - b;
+		},
+		"+": function(a, b) {
+			return a + b;
+		}
+	};
+	this.calculate = function(str) {
+		var split = str.split(' '),
+			a = +split[0],
+			symb = split[1],
+			b = +split[2];
+		if (!methods[symb] || isNaN(a) || isNaN(b)) {//ЕСЛИ нет символа, либо невозможно преваратить в число
+			return NaN;
+		}
+		return symbols[symb](+a, +b);
+	}
+	this.addMethod = function(name, func) {
+		symbols[name] = func;
+	};
+}
+var calc = new Calculator2;
+console.log(calc.calculate("3 + 7"));
+
+var powerCalc = new Calculator2;
+powerCalc.addMethod("*", function(a, b) {
+	return a * b;
+});
+powerCalc.addMethod("/", function(a, b) {
+	return a / b;
+});
+powerCalc.addMethod("**", function(a, b) {
+	return Math.pow(a, b);
+});
+var result = powerCalc.calculate("2 ** 3");
+console.log(result);

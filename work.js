5f10bd8441e97cc802722c7356586c8de5e4db38
KashaevaDasha <kashaeva1998@list.ru>
2016-09-01 22:55:22 +0300

    JS_14

--- work.js
+++ work.js
@@ -1,40 +1,131 @@
 window.onload = function() {
 
-	var pitch = document.getElementById('football-pitch');
-	var ball = document.getElementById('ball');
+	var table = drawTable();
+	var config = processTable(table);
+	var timer;
+	var count=0;
 
-	pitch.addEventListener('click', function() {
-		event = event || window.event;
-		eventTarger = event.targer || event.srcElement;
-		eventCurrentTarget = event.currentTarget || this;
+	function rand(min, max) {
+		min = min || 0;
+		max = max || 1;
+		return Math.floor((Math.random() * (max - min) + 0.5) + min);
+	}
 
-		var pitchRect = pitch.getBoundingClientRect();
-		var ballRect = ball.getBoundingClientRect();
-
-		var HALF_WIDTH_BALL = ballRect.width / 2;
-		var HALF_HEIGHT_BALL = ballRect.height / 2;
-		var MOVE_TO_X = event.offsetX - HALF_WIDTH_BALL; //позиция по Х: перемещение right, left
-		var MOVE_TO_Y = event.offsetY - HALF_HEIGHT_BALL; //позиция по Y: перемещение top, bottom
-		var SIZE_OF_BORDER = (pitchRect.width - pitch.clientWidth) / 2;
-
-		ball.style.left = event.offsetX - HALF_WIDTH_BALL + 'px';
-		ball.style.top = event.offsetY - HALF_HEIGHT_BALL + 'px';
-
-		//right
-		if (MOVE_TO_X >= (pitch.clientWidth - ballRect.width)) {
-			ball.style.left = pitch.clientWidth - ballRect.width + 'px';
+	function getColor() {
+		var rgb = [];
+		for (var i = 0; i < 3; i++) {
+			rgb.push(rand(0, 255));
 		}
-		//bottom
-		if (MOVE_TO_Y >= (pitch.clientHeight - ballRect.height)) {
-			ball.style.top = pitch.clientHeight - ballRect.height + 'px';
+		return rgb;
+	}
+
+	function getInvertColor(rgb) {
+		for (var i = 0; i < 3; i++) {
+			rgb[i] = 255 - rgb[i];
 		}
-		//left
-		if (MOVE_TO_X <= (pitch.clientLeft - SIZE_OF_BORDER)) {
-			ball.style.left = pitch.clientLeft - SIZE_OF_BORDER + 'px';
+		return 'rgb(' + rgb + ')';
+	}
+
+	function drawTable() {
+
+		var button = document.createElement('button');
+
+		var color = getColor();
+		button.textContent = 'Большая синяя кнопка!';
+		button.style.backgroundColor = 'rgb(' + color + ')';
+		document.body.appendChild(button);
+
+		var span = document.createElement('span');
+
+		span.textContent = 'Инвертируемый цвет кнопки!'
+		span.style.backgroundColor = getInvertColor(color);
+		document.body.appendChild(span);
+
+		button.addEventListener('click', function() {
+			if (!timer) {
+				animate(config, 50);
+			} else {
+				clearInterval(timer);
+				timer = false;
+			}
+		});
+
+		var table = document.createElement('table');
+
+		for (var i = 0; i < 10; i++) {
+			var tr = document.createElement('tr');
+			for (var j = 0; j < 10; j++) {
+				var td = document.createElement('td');
+				td.textContent = rand(0, 100);
+				td.style.backgroundColor = 'rgb(' + getColor() + ')';
+				tr.appendChild(td);
+			}
+			table.appendChild(tr);
 		}
-		//top
-		if (MOVE_TO_Y <= (pitch.clientTop - SIZE_OF_BORDER)) {
-			ball.style.top = pitch.clientTop - SIZE_OF_BORDER + 'px';
+		document.body.appendChild(table);
+
+		var rect = table.getBoundingClientRect();
+		table.style.position = 'absolute';
+		table.style.top = window.innerHeight / 2 - rect.height / 2 + 'px';
+		table.style.left = window.innerWidth / 2 - rect.width / 2 + 'px';
+		return table;
+	}
+
+	function processTable(t) {
+		var config = [];
+		var tds = t.getElementsByTagName('td');
+		for (var i = 0; i < tds.length; i++) {
+			config.push({
+				top: tds[i].offsetTop,
+				left: tds[i].offsetLeft,
+				width: tds[i].offsetWidth,
+				height: tds[i].offsetHeight,
+				td: tds[i]
+			});
+			do {
+				config[i].dirLeft = rand(-1, 1);
+				config[i].dirTop = rand(-1, 1);
+			} while (!config[i].dirLeft && !config[i].dirTop)
 		}
-	})
-}
+		for (i = 0; i < config.length; i++) {
+			config[i].td.style.position = 'absolute';
+			config[i].td.style.top = config[i].top + 'px';
+			config[i].td.style.left = config[i].left + 'px';
+		}
+		return config;
+	}
+
+
+	function animate(config, delay) {
+		var rect = table.getBoundingClientRect();
+		var widthDoc = document.documentElement.clientWidth;
+		var heightDoc = document.documentElement.clientHeight;
+		//count++;
+	console.log(window.screen.width, widthDoc);
+
+		timer = setInterval(function() {
+			for (var i = 0; i < config.length; i++) {
+				var top = parseFloat(config[i].td.style.top) + (config[i].dirTop * rand(1, 7));
+				var left = parseFloat(config[i].td.style.left) + (config[i].dirLeft * rand(1, 7));
+				if (left + config[i].width <= window.pageXOffset - rect.left || left >= widthDoc || top + config[i].height <= window.pageYOffset - rect.top || top >= heightDoc) {
+					config[i].td.addEventListener('transitionend', animate);
+						//console.log(count++);
+						//stop(count);
+					//config[i].td.style.display = 'none';
+				} else {
+					config[i].td.style.top = top + 'px';
+					config[i].td.style.left = left + 'px';
+				}
+			}
+
+		}, delay);
+	}
+
+	function stop(count) {
+		if (count >= 100) {
+			clearInterval(timer);
+			//table.style.display='none';
+		}
+	}
+
+};
